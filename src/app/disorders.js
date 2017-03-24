28f79a0cb317c6aa011575ebee1d154c4fd46e37
smturek <smturek@gmail.com>
2017-03-24 00:23:11 -0600

    added bootstrap and disorder data file

---
+++ src/app/disorders.js
@@ -0,0 +1,95 @@
+"use strict";
+exports.DISORDERS = {
+    adults: [
+        {
+            name: "ADHD"
+        },
+        {
+            name: "Anorexia"
+        },
+        {
+            name: "Anxiety"
+        },
+        {
+            name: "Borderline Personality"
+        },
+        {
+            name: "Bi-Polar"
+        },
+        {
+            name: "Bulimia"
+        },
+        {
+            name: "Depression"
+        },
+        {
+            name: "Disruptive Mood Dysregulation Disorder"
+        },
+        {
+            name: "Obsessive Compulsive Disorder"
+        },
+        {
+            name: "Post Traumatic Stress Disorder"
+        },
+        {
+            name: "Schizophrenia"
+        },
+        {
+            name: "Seasonal Affective Disorder"
+        },
+        {
+            name: "Substance and Alchohol Use"
+        },
+    ],
+    children: [
+        {
+            name: "ADHD"
+        },
+        {
+            name: "Anorexia"
+        },
+        {
+            name: "Anxiety"
+        },
+        {
+            name: "Borderline Personality"
+        },
+        {
+            name: "Bi-Polar"
+        },
+        {
+            name: "Bulimia"
+        },
+        {
+            name: "Depression"
+        },
+        {
+            name: "Disruptive Mood Dysregulation Disorder"
+        },
+        {
+            name: "Obsessive Compulsive Disorder"
+        },
+        {
+            name: "Post Traumatic Stress Disorder"
+        },
+        {
+            name: "Schizophrenia"
+        },
+        {
+            name: "Seasonal Affective Disorder"
+        },
+        {
+            name: "Selective Mutism"
+        },
+        {
+            name: "Self Harm"
+        },
+        {
+            name: "Substance and Alchohol Use"
+        },
+        {
+            name: "Tourettes"
+        },
+    ]
+};
+//# sourceMappingURL=disorders.js.map
